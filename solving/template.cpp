8428f5541e4bdcd449d7f66483d8fd9c77f95432
emily <snri.atomoxetine@proton.me>
2024-11-25 20:42:23 -0300

    improved template prime related functions

--- solving/template.cpp
+++ solving/template.cpp
@@ -1,4 +1,5 @@
 // author: logemi
+// github.com/atomoxetine
 
 #pragma GCC diagnostic ignored "-Wc++11-extensions"
 #pragma GCC diagnostic ignored "-Wc++17-extensions"
@@ -43,7 +44,6 @@ typedef pair<ld, ld> pdd;
 #define FORK(x, n) for (ll k = (x); k < (n); k++)
 #define FORL(x, n) for (ll l = (x); l < (n); l++)
 #define FORR(x, n) for (ll r = (x); r < (n); r++)
-#define FOR(k, x, n) for (ll(k) = (x); (k) < (n); (k)++)
 #define bflip(a, b) ((a) ^= (1ull) << (b))
 #define bset(a, b) ((a) |= (1ull) << (b))
 #define bclear(a, b) ((a) &= ~(1ull) << (b))
@@ -69,6 +69,7 @@ __attribute__((const)) T _gcd_internal(const T a, const T b) {
         return a;
     return _gcd_internal(b, a % b);
 }
+/// greatest common divisor of a and b
 template <typename T> __attribute__((const)) T gcd(const T a, const T b) {
     if (a > b)
         return _gcd_internal(a, b);
@@ -76,6 +77,7 @@ template <typename T> __attribute__((const)) T gcd(const T a, const T b) {
         return _gcd_internal(b, a);
     return a;
 }
+/// least common multiple of a and b
 template <typename T> __attribute__((const)) T lcm(const T a, const T b) {
     return a * (b / gcd(a, b));
 }
@@ -90,6 +92,7 @@ void extended_gcd(ll a, ll b, ll *x, ll *y) {
     *x = y_ - x_ * (b / a);
     *y = x_;
 }
+/// modular inverse aka a^(-1) (mod m)
 __attribute__((const)) ll mod_inv(const ll a, const ll m) {
     ll x, y;
     extended_gcd(a, m, &x, &y);
@@ -100,6 +103,7 @@ __attribute__((const)) ll mod_inv(const ll a, const ll m) {
         return x % m;
     }
 }
+/// generates integer exponentiation table
 __attribute__((const)) const array<array<ll, 63>, 20> gen_ipow() {
     array<array<ll, 63>, 20> arr;
     fill(arr[0].begin(), arr[0].end(), 0ll);
@@ -117,6 +121,7 @@ __attribute__((const)) const array<array<ll, 63>, 20> gen_ipow() {
     return arr;
 }
 static const array<array<ll, 63>, 20> _ipow = gen_ipow();
+/// fast integer exponentiation
 __attribute__((pure)) ll ipow(ll b, ll e) {
     if (b < _ipow.size())
         return _ipow[b][e];
@@ -129,6 +134,7 @@ __attribute__((pure)) ll ipow(ll b, ll e) {
     }
     return r;
 }
+/// returns list of primes and the sieve
 template <typename T>
 __attribute__((const)) pair<vector<T>, vector<bool>> get_primes(const ll n) {
     vector<bool> sieve(n + 1, true);
@@ -136,32 +142,59 @@ __attribute__((const)) pair<vector<T>, vector<bool>> get_primes(const ll n) {
     primes.reserve(1.1l * n / log((ld)n));
     sieve[0] = false;
     sieve[1] = false;
-    for (int i = 2; i < n + 1; i++) {
+    for (int i = 2; i <= sqrt(n + 1); i++) {
         if (!sieve[i])
             continue;
-        sieve[i] = true;
-        primes.push_back(i);
-        for (int x = 2 * i; x < n + 1; x += i)
+        for (int x = i * i; x < n + 1; x += i)
             sieve[x] = false;
+    }
+    for (int i = 2; i < n + 1; i++) {
+        if (sieve[i])
+            primes.push_back(i);
     }
     return make_pair(primes, sieve);
 }
+/// returns true if x is prime
 template <typename T>
-__attribute__((const)) multiset<T> prime_fact(const ll x,
-                                              const vector<T> &primes) {
+__attribute__((const)) bool is_prime(const ll x, const vector<T> &primes,
+                                     const vector<bool> &sieve) {
+
+    if (x < sieve.size())
+        return sieve[x];
+
+    ll max_p = 1 + sqrt(x);
+    for (T p : primes) {
+        if (p > max_p)
+            return true;
+        if (x % p == 0)
+            return false;
+    }
+
+    return true;
+}
+/// returns prime factorization of x
+template <typename T>
+__attribute__((const)) multiset<T>
+prime_fact(ll x, const vector<T> &primes, const vector<bool> &sieve) {
     multiset<T> fact;
     ll max_p = 1 + sqrt(x);
     for (T p : primes) {
-        if (p >= max_p)
+        if (p > max_p)
             break;
-        if (x % p == 0) {
+        while (x % p == 0) {
             fact.insert(p);
-            if (x / p >= max_p)
-                fact.merge(prime_fact(x / p, primes));
+            x /= p;
         }
+        max_p = 1 + sqrt(x);
     }
+
+    if (is_prime(x, primes, sieve)) {
+        fact.insert(x);
+    }
+
     return fact;
 }
+/// counts number of inversions in T array
 template <class T, class Compare = less_equal<T>>
 int inversions(T arr[], const int l, const int r) {
     if (l == r - 1)
@@ -208,23 +241,30 @@ int inversions(T arr[], const int l, const int r) {
 
 */
 
-void _preprocess_() {}
+void preprocess() {
 
-void _solve_test_case_() {}
+}
 
-// #define USE_TEST_CASES
+void test_case() {
 
-int main() {
+}
+
+#define IGNORE_CSTDIO
+#define USE_TEST_CASES
+
+int main(void) {
+#ifdef IGNORE_CSTDIO
     cin.tie(NULL)->sync_with_stdio(false);
-    _preprocess_();
+#endif
+    preprocess();
 #ifdef USE_TEST_CASES
     int tt;
     cin >> tt;
     while (tt--) {
-        _solve_test_case_();
+        test_case();
     }
 #else
-    _solve_test_case_();
+    test_case();
 #endif
     return 0;
 }
